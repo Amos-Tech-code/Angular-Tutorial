e4e78edee8346110985b21f996c73a478484ecc1
Amos-Tech-code <amosk5132@gmail.com>
2026-09-09 13:18:37 +0300

    first commit - Components communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,22 +6,11 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Forms';
 
-  user : { name: string; email: string } = {
-    name: '',
-    email: '',
-  }
+  tasks: string[] = ['Task 1', 'Task 2', 'Task 3'];
 
-  submitForm(form: NgForm) : void {
-    if (form.valid) {
-      console.log(form.value, this.user);
-    }
-  }
-
-  validateEmail() : boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(this.user.email);
+  deleteTask(task: string) {
+    this.tasks = this.tasks.filter(t => t !== task);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TaskComponent,
   ],
   imports: [
     BrowserModule,
