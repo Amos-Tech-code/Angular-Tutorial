649a9dfd1b87c27081a51ad9a45ae66bade6b213
Amos-Tech-code <amosk5132@gmail.com>
2026-09-11 15:12:55 +0300

    first commit - Angular Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,22 +6,7 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Forms';
 
-  user : { name: string; email: string } = {
-    name: '',
-    email: '',
-  }
-
-  submitForm(form: NgForm) : void {
-    if (form.valid) {
-      console.log(form.value, this.user);
-    }
-  }
-
-  validateEmail() : boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(this.user.email);
-  }
+  title = 'Angular Tutorial';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,11 +10,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
