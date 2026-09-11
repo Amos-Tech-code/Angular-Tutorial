ea0b8a2b77995d621496f47fdae6d1199737e544
Amos-Tech-code <amosk5132@gmail.com>
2026-09-11 22:05:55 +0300

    first commit - Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotesComponent} from "./components/notes/notes.component";
+import {NoteComponent} from "./components/note/note.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {AboutComponent} from "./components/about/about.component";
+import {AboutProjectsComponent} from "./components/about-projects/about-projects.component";
+import {AboutSponsorsComponent} from "./components/about-sponsors/about-sponsors.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "notes", pathMatch: "full"},
+  {path: "notes", component: NotesComponent},
+  {path: "notes/:id", title: "Single Note", component: NoteComponent},
+  {path: "about", title: "About", component: AboutComponent,
+    children: [
+      {
+        path: "projects",
+        title: "Projects",
+        component: AboutProjectsComponent
+      },
+      {
+        path: "sponsors",
+        title: "Sponsors",
+        component: AboutSponsorsComponent
+      }
+    ]
+  },
+  {path: "**", title: "404 - Page Not Found", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
