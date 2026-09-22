59e51a4fd45fd5d40a14c5fbf2c2a6d6c1d99d91
Amos-Tech-code <amosk5132@gmail.com>
2026-09-22 17:19:32 +0300

    first commit - UI structure basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {BootstrapTutorialComponent} from "./learning/bootstrap-tutorial/bootstrap-tutorial.component";
+import {HtmlCssComponent} from "./learning/html-css/html-css.component";
+import {HomeComponent} from "./learning/home/home.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'html-css', component: HtmlCssComponent},
+  {path: 'bootstrap-tutorial', component: BootstrapTutorialComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+import { BootstrapTutorialComponent } from './learning/bootstrap-tutorial/bootstrap-tutorial.component';
+import { HtmlCssComponent } from './learning/html-css/html-css.component';
+import { HomeComponent } from './learning/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BootstrapTutorialComponent,
+    HtmlCssComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
